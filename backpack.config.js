85b994ded307d4c637ece9dbbfe413559f82f9ce
Kananek T <info.dvgamer@gmail.com>
2019-02-17 22:09:17 +0700

    dist dev

--- backpack.config.js
+++ backpack.config.js
@@ -1,12 +1,12 @@
-const path = require('path')
+const { join } = require('path')
 
 module.exports = {
   webpack: (config, options) => {
     config.entry.main = './index.js'
+    config.output.path = join(process.cwd(), 'dist')
     if (options.env === 'production') {
       config.devtool = false
       config.plugins.splice(1, 1)
-      config.output.path = path.join(process.cwd(), 'dist')
     }
     return config
   }
